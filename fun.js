04a9e7f180d79462c31bb9bec1139f9940a707cf
dheerajpal6018-lab <dheerajpal6018@gmail.com>
2026-09-23 11:49:24 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,28 @@
+// Function f1
+function f1() {
+    console.log("Function f1");
+}
+
+// Function f2
+function f2() {
+    console.log("Function f2");
+}
+
+// Function f3
+function f3() {
+    console.log("Function f3");
+}
+
+// Main function
+function main() {
+    console.log("Main function starts");
+
+    f1();
+   f2();
+    f3();
+
+    console.log("Main function ends");
+}
+
+// Call the main function
+main();
